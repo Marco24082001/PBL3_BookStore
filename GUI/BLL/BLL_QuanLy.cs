00402faeb7675cb81a42179f0d6959f6d34ac651
= <=>
2021-05-27 10:19:23 +0700

    27-05-2021

--- GUI/BLL/BLL_QuanLy.cs
+++ GUI/BLL/BLL_QuanLy.cs
@@ -6,6 +6,8 @@ using System.Data.Entity.Core.Objects;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms.DataVisualization.Charting;
+
 namespace GUI.BLL
 {
     class BLL_QuanLy
@@ -93,6 +95,10 @@ namespace GUI.BLL
             List<SACH> lSach = list;
             return lSach;
         }
+        public List<SACH> Bll_GetAllSachByTrangThai()
+        {
+            return db.SACHes.Where(p => p.TrangThai == true).ToList();
+        }
         public List<DOANH_SO_BAN_HANG> BLL_GetAllDoanhSoBanHang()
         {
             return db.DOANH_SO_BAN_HANG.ToList();
@@ -111,6 +117,11 @@ namespace GUI.BLL
             List<String> list = new List<string>();
             list = db.SACHes.Select(p => p.MaSach).ToList();
             return list;
+        }
+
+        public List<string> Bll_GetAllMaSachByTrangThai()
+        {
+            return db.SACHes.Where(p => p.TrangThai).Select(a => a.MaSach).ToList();
         }
 
         public List<string> Bll_GetAllMaDonNhap()
@@ -441,72 +452,72 @@ namespace GUI.BLL
             return db.DOANH_SO_BAN_HANG.Where(p => p.ThoiGian.Value.Year == nam && p.ThoiGian.Value.Month == thang && p.ThoiGian.Value.Day == ngay).ToList();
         }
 
-        public SeriesCollection Bll_GetValueChart(int num1, int num2, int num3 = -1)
-        {
-            SeriesCollection series = new SeriesCollection();
-            //var years = db.DOANH_SO_BAN_HANG.Select(p => new { Year = p.ThoiGian.Value.Year }).Distinct();
+        //public SeriesCollection Bll_GetValueChart(int num1, int num2, int num3 = -1)
+        //{
+        //    SeriesCollection series = new SeriesCollection();
+        //    //var years = db.DOANH_SO_BAN_HANG.Select(p => new { Year = p.ThoiGian.Value.Year }).Distinct();
 
-            //foreach (var year in years)
-            //{
-            //    List<int> values = new List<int>();
-            //    for (int month = 1; month <= 11; month++)
-            //    {
-            //        int value = 0;
-            //        var data = db.DOANH_SO_BAN_HANG.Where(p => p.ThoiGian.Value.Year.Equals(year.Year) && p.ThoiGian.Value.Month.Equals(month))
-            //                                       .Select(p => new { p.DoanhSoBan, p.ThoiGian.Value.Month })
-            //                                       .GroupBy(p => p.Month)
-            //                                       .Select(p => new { Month = p.Key, SumTurnover = p.Sum(i => i.DoanhSoBan) });
-            //        if (data.SingleOrDefault() != null)
-            //            value = (int)data.SingleOrDefault().SumTurnover;
-            //        values.Add(value);
-            //    }
-            //    series.Add(new LineSeries() { Title = year.Year.ToString(), Values = new ChartValues<int>(values) });
-            //}
-            //return series;
-            if (num3 != -1)
-            {
-                var months = db.DOANH_SO_BAN_HANG.Where(p => p.ThoiGian.Value.Year.Equals(num3) && p.ThoiGian.Value.Month.Equals(num1) || p.ThoiGian.Value.Month.Equals(num2))
-                                                 .Select(p => new { Month = p.ThoiGian.Value.Month }).Distinct();
-                foreach(var month in months)
-                {
-                    List<int> values = new List<int>();
-                    for (int day = 1; day <= 31; day++)
-                    {
-                        int value = 0;
-                        var data = db.DOANH_SO_BAN_HANG.Where(p => p.ThoiGian.Value.Year.Equals(num3) && p.ThoiGian.Value.Month.Equals(month.Month) && p.ThoiGian.Value.Day.Equals(day))
-                                                       .Select(p => new { p.DoanhSoBan, p.ThoiGian.Value.Day })
-                                                       .GroupBy(p => p.Day)
-                                                       .Select(p => new { Day = p.Key, SumTurnover = p.Sum(i => i.DoanhSoBan) });
-                        if (data.SingleOrDefault() != null)
-                            value = (int)data.SingleOrDefault().SumTurnover;
-                        values.Add(value);
-                    }
-                    series.Add(new LineSeries() { Title = month.Month.ToString(), Values = new ChartValues<int>(values) });
-                }
-                return series;
-            }
-            else
-            {
-                var years = db.DOANH_SO_BAN_HANG.Where(p => p.ThoiGian.Value.Year.Equals(num1) || p.ThoiGian.Value.Year.Equals(num2))
-                                                .Select(p => new { Year = p.ThoiGian.Value.Year }).Distinct();
-                foreach(var year in years)
-                {
-                    List<int> values = new List<int>();
-                    for (int month = 1; month <= 12; month++)
-                    {
-                        int value = 0;
-                        var data = db.DOANH_SO_BAN_HANG.Where(p => p.ThoiGian.Value.Year.Equals(year.Year) && p.ThoiGian.Value.Month.Equals(month))
-                                                       .Select(p => new { p.DoanhSoBan, p.ThoiGian.Value.Month })
-                                                       .GroupBy(p => p.Month)
-                                                       .Select(p => new { Month = p.Key, SumTurnover = p.Sum(i => i.DoanhSoBan) });
-                        if (data.SingleOrDefault() != null)
-                            value = (int)data.SingleOrDefault().SumTurnover;
-                        values.Add(value);
-                    }
-                    series.Add(new LineSeries() { Title = year.Year.ToString(), Values = new ChartValues<int>(values) });
-                }
-                return series;
-            }
-        }
+        //    //foreach (var year in years)
+        //    //{
+        //    //    List<int> values = new List<int>();
+        //    //    for (int month = 1; month <= 11; month++)
+        //    //    {
+        //    //        int value = 0;
+        //    //        var data = db.DOANH_SO_BAN_HANG.Where(p => p.ThoiGian.Value.Year.Equals(year.Year) && p.ThoiGian.Value.Month.Equals(month))
+        //    //                                       .Select(p => new { p.DoanhSoBan, p.ThoiGian.Value.Month })
+        //    //                                       .GroupBy(p => p.Month)
+        //    //                                       .Select(p => new { Month = p.Key, SumTurnover = p.Sum(i => i.DoanhSoBan) });
+        //    //        if (data.SingleOrDefault() != null)
+        //    //            value = (int)data.SingleOrDefault().SumTurnover;
+        //    //        values.Add(value);
+        //    //    }
+        //    //    series.Add(new LineSeries() { Title = year.Year.ToString(), Values = new ChartValues<int>(values) });
+        //    //}
+        //    //return series;
+        //    if (num3 != -1)
+        //    {
+        //        var months = db.DOANH_SO_BAN_HANG.Where(p => p.ThoiGian.Value.Year.Equals(num3) && p.ThoiGian.Value.Month.Equals(num1) || p.ThoiGian.Value.Month.Equals(num2))
+        //                                         .Select(p => new { Month = p.ThoiGian.Value.Month }).Distinct();
+        //        foreach(var month in months)
+        //        {
+        //            List<int> values = new List<int>();
+        //            for (int day = 1; day <= 31; day++)
+        //            {
+        //                int value = 0;
+        //                var data = db.DOANH_SO_BAN_HANG.Where(p => p.ThoiGian.Value.Year.Equals(num3) && p.ThoiGian.Value.Month.Equals(month.Month) && p.ThoiGian.Value.Day.Equals(day))
+        //                                               .Select(p => new { p.DoanhSoBan, p.ThoiGian.Value.Day })
+        //                                               .GroupBy(p => p.Day)
+        //                                               .Select(p => new { Day = p.Key, SumTurnover = p.Sum(i => i.DoanhSoBan) });
+        //                if (data.SingleOrDefault() != null)
+        //                    value = (int)data.SingleOrDefault().SumTurnover;
+        //                values.Add(value);
+        //            }
+        //            series.Add(new LineSeries() { Title = month.Month.ToString(), Values = new ChartValues<int>(values) });
+        //        }
+        //        return series;
+        //    }
+        //    else
+        //    {
+        //        var years = db.DOANH_SO_BAN_HANG.Where(p => p.ThoiGian.Value.Year.Equals(num1) || p.ThoiGian.Value.Year.Equals(num2))
+        //                                        .Select(p => new { Year = p.ThoiGian.Value.Year }).Distinct();
+        //        foreach(var year in years)
+        //        {
+        //            List<int> values = new List<int>();
+        //            for (int month = 1; month <= 12; month++)
+        //            {
+        //                int value = 0;
+        //                var data = db.DOANH_SO_BAN_HANG.Where(p => p.ThoiGian.Value.Year.Equals(year.Year) && p.ThoiGian.Value.Month.Equals(month))
+        //                                               .Select(p => new { p.DoanhSoBan, p.ThoiGian.Value.Month })
+        //                                               .GroupBy(p => p.Month)
+        //                                               .Select(p => new { Month = p.Key, SumTurnover = p.Sum(i => i.DoanhSoBan) });
+        //                if (data.SingleOrDefault() != null)
+        //                    value = (int)data.SingleOrDefault().SumTurnover;
+        //                values.Add(value);
+        //            }
+        //            series.Add(new LineSeries() { Title = year.Year.ToString(), Values = new ChartValues<int>(values) });
+        //        }
+        //        return series;
+        //    }
+        //}
     }   
 }
