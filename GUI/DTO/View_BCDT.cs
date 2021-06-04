5bc05f1bd0a3225dfc2f59cb77c0e415d69d480d
= <=>
2021-06-04 23:01:03 +0700

    Commit DTO

---
+++ GUI/DTO/View_BCDT.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI.DTO
+{
+    class View_BCDT
+    {
+        public int STT { get; set; }
+        public string TenSach { get; set; }
+        public int GiaNhap { get; set; }
+        public int GiaBan { get; set; }
+        public int SoLuong { get; set; }
+        public int DoanhThu { get; set; }
+        public DateTime ThoiGian { get; set; }
+    }
+}
---
+++ GUI/DTO/View_Sach.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI.DTO
+{
+    class View_Sach
+    {
+        public string MaSach { get; set; }
+        public string TenSach { get; set; }
+        public int GiaBan { get; set; }
+        public int SoLuong { get; set; }
+        public string TenNXB { get; set; }
+        public string TenLS { get; set; }
+    }
+}
